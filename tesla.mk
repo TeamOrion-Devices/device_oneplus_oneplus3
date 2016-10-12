82033d26353826a211d39bb838fb1e345c517dc8
Martinusbe <martinusbe@gmail.com>
2016-10-12 09:59:51 +0200

    op3: use Tesla vendor caf_fw.mk
    Now needed to build telephony-ext on CAF devices, since it bootlooped shamu
    when it was in common_full_phone.mk
    
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- tesla.mk
+++ tesla.mk
@@ -21,6 +21,7 @@ $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit some common Tesla stuff.
 $(call inherit-product, vendor/tesla/config/common_full_phone.mk)
+$(call inherit-product, vendor/tesla/config/caf_fw.mk)
 
 PRODUCT_NAME := tesla_oneplus3
 PRODUCT_DEVICE := oneplus3
