75a6e98fde8525aa16d66952f6301f6f7088249b
Sultanxda <sultanxda@gmail.com>
2016-11-05 15:29:14 -0700

    oneplus3: Remove camera shim
    Don't need this.
    
    Change-Id: I9b43772cb6693f6fb2b297495850dc91d543bd8b
    Signed-off-by: bgos10 <bishalgoswami@gmail.com>

--- device.mk
+++ device.mk
@@ -141,10 +141,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-    libcamera_shim
-
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
