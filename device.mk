01ae1194f68e90e46c5373faaa8be028682468c7
Martinusbe <martinusbe@gmail.com>
2016-11-21 16:14:32 +0100

    Revert "oneplus3: add doze package"
    This reverts commit c7b6808034acf38433132b6d0345e1a5a97762c2.
    
    Signed-off-by: Martinusbe <martinusbe@gmail.com>
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -165,10 +165,6 @@ PRODUCT_PACKAGES += \
     FOSSConfig.xml \
     qdcm_calib_data_samsung_s6e3fa3_1080p_cmd_mode_dsi_panel.xml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    OneplusDoze
-
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     fingerprintd \
